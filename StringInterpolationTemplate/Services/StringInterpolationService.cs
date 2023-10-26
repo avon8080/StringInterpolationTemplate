770ca837cb421c44b71f5690bd96e83525524ba0
avon8080 <evanjlalor@gmail,com>
2023-10-26 14:10:19 -0500

    assignment tests

--- StringInterpolationTemplate/Services/StringInterpolationService.cs
+++ StringInterpolationTemplate/Services/StringInterpolationService.cs
@@ -16,30 +16,6 @@ public class StringInterpolationService : IStringInterpolationService
         _logger.Log(LogLevel.Information, "Executing the StringInterpolationService");
     }
 
-    //1. January 22, 2019 (right aligned in a 40 character field)
-    //1.January 22, 2019 (right aligned in a 40 character field)
-
-    //2.2019.01.22
-
-    //3.Day 22 of January, 2019
-
-    //    4.Year: 2019, Month: 01, Day: 22
-
-    //5.            Tuesday(10 spaces total, right aligned)
-
-    //6.     11:01 PM Tuesday(10 spaces total for each including the day-of-week, both right-aligned)
-
-    //7.h:11, m:01, s:27
-
-    //8.2019.01.22.11.01.27
-
- 
-
-    //If you have PI(3.1415) - use var pi = Math.PI;
-
-    //1. Output as currency
-
-    //2. Output as right-aligned(10 spaces), number with 3 decimal places
     public string Number01()
     {
         var date = _date.Now.ToString("MMMM dd, yyyy");
